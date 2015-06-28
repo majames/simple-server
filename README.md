94cd6a53051e7f6a2db4ced80d6b92f84306329a
majames <majames91@gmail.com>
2015-06-28 10:54:33 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple HTTP server I use for testing React.js jsx and typekit fonts locally.
---
+++ server.rb
@@ -0,0 +1,79 @@
+require 'socket'
+require 'uri'
+
+WEB_ROOT = '/home/majames/Documents/web_dev/godhesbig'
+
+CONTENT_TYPE_MAPPING = {
+  html: 'text/html',
+  txt: 'text/plain',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  css: 'text/css',
+  less: 'text/css',
+  js: 'application/javascript'
+}
+
+DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+
+def content_type(path)
+  ext = File.extname(path).split(".").last
+  CONTENT_TYPE_MAPPING.fetch(ext.to_sym, DEFAULT_CONTENT_TYPE)
+end
+
+def requested_file(request_line)
+  request_uri  = request_line.split(" ")[1]
+  path         = URI.unescape(URI(request_uri).path)
+
+  clean = []
+
+  parts = path.split("/")
+
+  parts.each do |part|
+    next if part.empty? || part == '.'
+
+    part == '..' ? clean.pop : clean << part
+  end
+
+  STDERR.print File.join(WEB_ROOT, *clean)
+  File.join(WEB_ROOT, *clean)
+end
+
+server = TCPServer.new('localhost', 2345)
+
+loop do
+  socket = server.accept
+  request_line = socket.gets
+
+  STDERR.puts request_line
+
+  path = requested_file(request_line)
+
+  path = File.join(path, 'index.html') if File.directory?(path)
+
+  if File.exist?(path) && !File.directory?(path)
+    File.open(path, 'rb') do |file|
+      socket.print "HTTP/1.1 200 OK\r\n" +
+                   "Content-Type: #{content_type(file)}\r\n" +
+                   "Content-Length: #{file.size}\r\n" +
+                   "Connection: close\r\n"
+
+      socket.print "\r\n"
+
+      IO.copy_stream(file, socket)
+    end
+  else
+    message = "File not found\n"
+
+    socket.print "HTTP/1.1 404 Not Found\r\n" +
+                 "Content-Type: text/plain\r\n" +
+                 "Content-Length: #{message.size}\r\n" +
+                 "Connection: close\r\n"
+
+    socket.print "\r\n"
+
+    socket.print message
+  end
+
+  socket.close
+
+end
